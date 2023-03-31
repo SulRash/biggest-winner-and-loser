aa784cebe38d9636214c49deaa41e62f9cffc4bb
Sultan <92447824+SulRash@users.noreply.github.com>
2023-03-31 21:50:49 +0300

    Switched to using VantageAPI
    Seems to work but I need to work on things more.

--- background.js
+++ background.js
@@ -1,21 +1,39 @@
+// background.js (Alpha Vantage)
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "fetchStockData") {
-      const symbol = "AAPL";
+      const symbols = ["AAPL", "GOOG", "TSLA", "MSFT", "AMZN"]; // replace with your own symbols
+      const apiKey = "demo"; // replace with your own API key
+      const apiUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=${apiKey}`;
   
-      fetch(`https://finviz.com/quote.ashx?t=${symbol}`)
-        .then((response) => response.text())
-        .then((text) => {
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(text, "text/html");
-          const priceChangeElement = doc.querySelector(
-            "table.snapshot-table2 td:nth-child(11)"
-          );
-          const priceChangeText = priceChangeElement.textContent;
-          const change = parseFloat(priceChangeText.replace(/[+%]/g, ""));
-          sendResponse({ symbol, change });
+      const fetchPromises = symbols.map((symbol) => {
+        const url = `${apiUrl}&symbol=${symbol}`;
+        return fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.hasOwnProperty("Global Quote")) {
+              const priceChangeText = data["Global Quote"]["10. change percent"];
+              const change = parseFloat(priceChangeText.replace(/[+%]/g, ""));
+              return { symbol, change };
+            } else {
+              console.error(`Error fetching data for ${symbol}:`, data);
+              return { symbol, error: "Error fetching data" };
+            }
+          })
+          .catch((error) => {
+            console.error(`Error fetching data for ${symbol}:`, error);
+            return { symbol, error: "Error fetching data" };
+          });
+      });
+  
+      Promise.all(fetchPromises)
+        .then((results) => {
+          const sortedResults = results.sort((a, b) => b.change - a.change);
+          const topGainers = sortedResults.slice(0, 10);
+          const topLosers = sortedResults.slice(-10).reverse();
+          sendResponse({ topGainers, topLosers });
         })
         .catch((error) => {
-          console.error(`Error fetching data for ${symbol}:`, error);
+          console.error(`Error fetching data:`, error);
           sendResponse({ error: "Error fetching data" });
         });
   
